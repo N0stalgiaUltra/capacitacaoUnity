607cd8bee71ab6720f332a7fe1ee12743213843d
Vinicius Santos <viniciusantos0898@gmail.com>
2021-09-24 19:33:38 -0300

    Concluida a movimentação do player

---
+++ Assets/Scripts/New Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Vector2 movimento;
+    public float velocidade;
+
+    public bool estaChao;
+    void Start()
+    {
+        estaChao = false;
+    }
+
+    void Update()
+    {
+
+        /*** Movimento do Player ***/ 
+        
+        movimento.x = Input.GetAxisRaw("Horizontal");
+        movimento.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movimento.x * velocidade, rb.velocity.y);
+
+        if(estaChao)
+            rb.velocity = new Vector2(rb.velocity.x, movimento.y * velocidade);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Chao")
+            estaChao = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Chao")
+            estaChao = false;
+    }
+}
+
+
+//movimento.y * velocidade
---
+++ Assets/Scripts/Old Scripts/Enemy/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime);
+
+    }
+}
